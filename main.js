9d00578743407336e363adcfc236cbdca00fa2cb
Federico <alegriafederico0@gmail.com>
2024-08-27 19:09:32 -0600

    :sparkles: feat: consume rick&morty API

--- main.js
+++ main.js
@@ -1,37 +1,27 @@
-/* Promise()
-//Variables de estados
-pending INICIAL
-fulfilled Operacion exitosa
-rejected Operacion fallida */
+//Rick & Morty API
+const endpoint = 'https://rickandmortyapi.com/api/character';
+const app = document.getElementById("app");
 
-//Tipos de callbacks
-/* resolve Operacion exitosa
-reject Operacion Fallida
-finally Se ejecuta pase lo que pase */
+fetch(endpoint)
+  .then(response => response.json())
+  .then(data => {
+    const characters = data.results;
 
-// Metodos de una promesa
-/* .then cuando la Operacion es exitosa
-.catch() Operacion Fallida */
-
-const myPromise = new Promise((resolve, reject) => {
-
-  setTimeout(() => {
-    let operationSucces = false;
-    if(operationSucces){
-      resolve('Operacion exitosa')
-    } else {
-      reject('Operacion Fallida')
-    }
-
-
-  }, 3000);  
-
-})
-
-console.log(myPromise);
-
-myPromise.then((message) => {
-  console.log(message)
-}).catch((error) => {
-  console.log(error)
-})
+    characters.map(character => {
+      const characterElement = document.createElement('div')
+      
+      characterElement.innerHTML = `
+        <h2>${character.name}</h2>
+        <h4>${character.status}</h4>
+        <h4>${character.species}</h4>
+        <h4>${character.gender}</h4>
+        <h2>${character.origin.name}</h2>
+        <img src="${character.image}" alt="${character.name}">
+      `
+      
+      app.appendChild(characterElement);
+    }) 
+  })
+  .catch((error) => {
+    console.log(error)
+  })
